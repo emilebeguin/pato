ea00067ae7ff8d55301c2a0e1fd922ba9468dd94
Émile Beguin <milleetunenuit@gmail.com>
2022-01-21 17:02:11 +0100

    [version 0.0] side nav parfaite

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,12 +9,22 @@ let sideWrapper = document.createElement("aside");
 let sideNav = document.createElement("nav");
 sideNav.classList.add('side');
 
+// transfo du burger en cross
+let cross = burger.cloneNode(true);
+
+// construction du side
 sideWrapper.appendChild(sideNav);
-sideNav.appendChild(burger);
+sideNav.appendChild(cross);
 sideNav.appendChild(navlinks);
 sideNav.appendChild(gallery);
 document.body.insertBefore(sideWrapper, document.body.firstChild);
 
-burger.addEventListener('click', function() {
-    sideWrapper.classList.toggle("active");
+let showSide = document.querySelectorAll('.burger');
+
+// event listeners
+showSide.forEach(clicker => {
+    clicker.addEventListener('click', function() {
+        console.log("hello");
+        sideWrapper.classList.toggle("active");
+    });
 });
